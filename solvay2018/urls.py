95f4bf250ab97728ccd84adf9288b8689f5591f9
Alexandre Gonze <alexandre@gonze.be>
2018-04-06 20:55:50 +0200

    change to 1.11

--- solvay2018/urls.py
+++ solvay2018/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from solvay2018.views import *
+from django.conf.urls import url
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
